879f93a2d87895cb996b50bacfdc5d7cf6e81012
sojalpatil <sojalpatil1999@gmail.com>
2022-10-27 17:21:11 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Contact from './components/contacts/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
     <>
         <Header />
         <Nav />
+        <About />
+        <Experience />
+        <Contact />
+        <Footer />
     </>
   )
 }
